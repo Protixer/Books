85609a5a72a34a19f7299570a2e3b8226ada395d
Daarkey <dimitrova_moni@yahoo.com>
2024-01-27 22:27:45 +0200

    Adding 2nd and 3rd chapter

---
+++ Python Crash Course 3rd Edition - Eric Matthes/Darkey7/chapter2.py
@@ -0,0 +1,38 @@
+#2-3
+
+# name = input()
+
+# print(f'Hello {name},would you like to learn some Python today?')
+
+#2-4
+
+# name = input()
+
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+
+#2-5 + 2-6
+
+# name = "albert einstein"
+# quote = "A person who never made a mistake never tried anything new"
+
+# print(f'{name.title()} once said, "{quote}"')
+
+#2-7
+
+# name = " bonboni "
+
+# print(name)
+# print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+
+#2-8
+
+# filename = "python_notes.txt"
+
+# print(filename.removesuffix(".txt"))
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://'))
