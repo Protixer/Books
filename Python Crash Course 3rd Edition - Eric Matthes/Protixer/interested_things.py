c878f735c3b3e55e50a7b23c1262565c1600b43b
Protixer <gambler4o0o0@gmail.com>
2024-01-28 16:10:58 +0200

    Initial commit

---
+++ Python Crash Course 3rd Edition - Eric Matthes/Protixer/interested_things.py
@@ -0,0 +1,6 @@
+"""A constant is a variable whose value stays the same throughout the life of a
+program. Python doesn’t have built-in constant types, but Python programmers 
+use all capital letters to indicate a variable should be treated as a constant and never be changed:"""
+
+# MAX_CONNECTIONS = 5000
+# INCREASE = 1
---
+++ Python Crash Course 3rd Edition - Eric Matthes/Protixer/list.py
@@ -0,0 +1,19 @@
+# names = ["Simona","Misho","Oleg","Venci","Toshiqta"]
+# mess = f"{names[0]} have good tits!"
+# print(mess)
+
+"""Change list values"""
+# names = ["Simona","Misho","Oleg","Venci","Toshiqta"]
+
+# for i in range(len(names)):
+#     names[i] = input()
+
+# print(f"List:{names}")
+
+"""Add elements to list"""
+names = ["Simona","Misho","Oleg","Venci","Toshiqta"]
+
+names.append("Gosho")
+names.append("Blzxa")
+
+print(f"List:{names}")
