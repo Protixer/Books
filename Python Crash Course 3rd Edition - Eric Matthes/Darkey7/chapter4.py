3d71eaf0609e89cc9cd44bd18d272a69c863e5f2
Daarkey <dimitrova_moni@yahoo.com>
2024-02-04 22:33:18 +0200

    Updated chapter 4

--- Python Crash Course 3rd Edition - Eric Matthes/Darkey7/chapter4.py
+++ Python Crash Course 3rd Edition - Eric Matthes/Darkey7/chapter4.py
@@ -2,8 +2,79 @@
 # for magician in magicians:
 #     print(magician)
 
-magicians = ['alice', 'david', 'carolina']
-for magician in magicians:
-    print(f"{magician.title()}, that was a great trick!")
-    print(f"I can't wait to see your next trick, {magician.title()}.\n")
-print("Thank you, everyone. That was a great magic show!")
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(f"{magician.title()}, that was a great trick!")
+#     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+# print("Thank you, everyone. That was a great magic show!")
+
+
+# 4-1
+
+# pizza_list = ["margarita", "formaggie", "vegetarian"]
+
+# for pizza in pizza_list:
+#     print(f"I like {pizza} pizza.")
+
+# print("I really love pizza!!!")
+
+# 4-2
+
+# animals_list = ["cat", "dog", "bunny"]
+
+# for animal in animals_list:
+#     print(f"A {animal} would make a great pet.")
+
+# print("Any of these animals would make a great pet!!")
+
+
+# numbers = list(range(1, 6))
+# print(numbers)
+
+
+# even_numbers = list(range(2, 11, 2))
+# print(even_numbers)
+
+
+# squares = []
+# for value in range(1, 11):
+#     squares.append(value**2)
+# print(squares)
+
+
+# LIST COMPREHENSION
+
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+
+# 4-3
+# for num in range(1, 21):
+#     print(num)
+
+
+# 4-4 and 4-5
+
+# million_numbers = list(range(1, 1000001))
+
+# print(min(million_numbers))
+# print(max(million_numbers))
+# print(sum(million_numbers))
+
+# # 4-6
+# for odd_num in range(1, 21, 2):
+#     print(odd_num)
+
+# 4-7
+# for num in range(3, 31):
+#     print(num * 3)
+
+
+# 4-8
+# for num in range(1, 11):
+#     print(num**3)
+
+
+# 4-9
+# list_of_cubes = [num**3 for num in range(1, 11)]
+# print(list_of_cubes)
