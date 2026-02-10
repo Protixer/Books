770ba5f292d50299eb2c7444e03998b43828d562
Protixer <gambler4o0o0@gmail.com>
2026-02-10 17:19:47 +0000

    Add initial project files and structure

---
+++ Python Crash Course 3rd Edition - Eric Matthes/print.py
@@ -0,0 +1,12 @@
+print("hello world".title())
+print("bye bye".title())
+# print every work with uppercase first symbol, not every symbol
+print("hello world i am your papa".title())
+print("i like reyna,viper,chamber,omen.".title())
+
+valorant_duelist_name = 'Reyna'
+print(valorant_duelist_name.upper())
+valorant_duelist_name = 'Viper'
+print(valorant_duelist_name.lower())
+print('Reyna said this:"You gonna die all."')
+print("Chamber:'You want to play? Let's play.'")
