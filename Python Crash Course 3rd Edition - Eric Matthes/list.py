6bb08e46795b53974d1eb5c533b8e3c4d4049f35
Protixer <gambler4o0o0@gmail.com>
2026-02-15 18:11:38 +0000

    Learning Lists. Modifying Elements in a List.

--- Python Crash Course 3rd Edition - Eric Matthes/list.py
+++ Python Crash Course 3rd Edition - Eric Matthes/list.py
@@ -1,5 +1,16 @@
-valorant_agents = ['Jett', 'Sage', 'Raze', 'Breach', 'Omen']
+valorant_agents = ['Jett', 'Sage', 'Raze', 'Breach', 'Omen', 'chamber']
 print(valorant_agents)
 print(valorant_agents[0])
 print(valorant_agents[1:4])
 print(valorant_agents[-1])
+print(valorant_agents[-2])
+print(f'Best valorant agent is {valorant_agents[-1].title()}')
+
+valorant_agents[1] = 'Reyna'
+print(valorant_agents)
+
+for agent in valorant_agents:
+    print(agent.title())
+
+valorant_agents.append('Sky')
+print(valorant_agents)
