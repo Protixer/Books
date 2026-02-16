20f9dc87e9e51b84d34f43f79bc7669430f03b73
Protixer <gambler4o0o0@gmail.com>
2026-02-16 20:56:17 +0000

    Learning Lists. Modifying Elements in a List.

--- Python Crash Course 3rd Edition - Eric Matthes/list.py
+++ Python Crash Course 3rd Edition - Eric Matthes/list.py
@@ -1,19 +1,40 @@
 valorant_agents = ['Jett', 'Sage', 'Raze', 'Breach', 'Omen', 'chamber']
-print(valorant_agents)
-print(valorant_agents[0])
-print(valorant_agents[1:4])
-print(valorant_agents[-1])
-print(valorant_agents[-2])
-print(f'Best valorant agent is {valorant_agents[-1].title()}')
+# print(valorant_agents)
+# print(valorant_agents[0])
+# print(valorant_agents[1:4])
+# print(valorant_agents[-1])
+# print(valorant_agents[-2])
+# print(f'Best valorant agent is {valorant_agents[-1].title()}')
+#
+# valorant_agents[1] = 'Reyna'
+# print(valorant_agents)
+#
+# for agent in valorant_agents:
+#     print(agent.title())
+#
+# valorant_agents.append('Sky')
+# print(valorant_agents)
+#
+# valorant_agents.insert(0, 'Viper')
+# print(valorant_agents)
 
-valorant_agents[1] = 'Reyna'
-print(valorant_agents)
+# Sorted does not change the original list, it returns a new sorted list
+# sorted_valorant_agents = sorted(valorant_agents)
+# print(valorant_agents)
+# print(sorted_valorant_agents)
+# print(valorant_agents)
 
-for agent in valorant_agents:
-    print(agent.title())
+# # Sort method changes the original list
+# sort_valorant_agents = valorant_agents.sort()
+# print(valorant_agents.sort())
+# print(sort_valorant_agents)
+# print(valorant_agents)
 
-valorant_agents.append('Sky')
-print(valorant_agents)
+# Reverse method changes the original list
+# numbers = [1, 2, 3, 4, 5]
+# numbers.sort(key=lambda x: x,reverse=True)
+# print(numbers)
+# print(len(numbers))
+# numbers.reverse()
+# print(numbers)
 
-valorant_agents.insert(0, 'Viper')
-print(valorant_agents)
