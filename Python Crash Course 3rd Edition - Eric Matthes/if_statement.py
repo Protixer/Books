91a31c267422370f3e3f05b7db080898ec8ff069
protixer <gambler4o0o0@gmail.com>
2026-02-26 23:18:57 +0200

    Learning conditional statements

--- Python Crash Course 3rd Edition - Eric Matthes/if_statement.py
+++ Python Crash Course 3rd Edition - Eric Matthes/if_statement.py
@@ -36,3 +36,12 @@
 # print(car == "subaru")
 # print("\nIs car == 'audi'? I predict False.")
 # print(car == "audi")
+
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0")
+elif age < 18:
+    print("Your admission cost is $50")
+else:
+    print("Your admission cost is $100")
