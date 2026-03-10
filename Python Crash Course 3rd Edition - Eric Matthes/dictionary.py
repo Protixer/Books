d4ec840847b403c0e728a2531cd73fdb4e7e8c72
Protixer <60284820+Protixer@users.noreply.github.com>
2026-03-10 22:08:36 +0200

    Learning dictionary

--- Python Crash Course 3rd Edition - Eric Matthes/dictionary.py
+++ Python Crash Course 3rd Edition - Eric Matthes/dictionary.py
@@ -1,9 +1,23 @@
-alien_0 = {'color': 'green',
-           'points': 5 }
+# alien_0 = {'color': 'green',
+#            'points': 5 }
+#
+# print(alien_0['color'])
+# print(alien_0['points'])
+#
+# alien_0 = {'color':'purple'}
+# print(alien_0)
+# print(alien_0['color'])
+#
+# alien_0['score'] = 10
+# print(alien_0)
+# alien_0['points'] = 5
+# print(alien_0)
+# print(alien_0['points'])
+# print(alien_0['score'])
+#
 
-print(alien_0['color'])
-print(alien_0['points'])
-
-alien_0 = {'color':'purple'}
-print(alien_0)
-print(alien_0['color'])
+valorant_agents = {}
+valorant_agents['duelist'] = 'Jet'
+valorant_agents['controller'] = 'Brim'
+valorant_agents['sentinel'] = 'Breach'
+print(valorant_agents)
