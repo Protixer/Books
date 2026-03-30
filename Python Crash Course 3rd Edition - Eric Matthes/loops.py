962ad6e7f6c61a11cad680087226115ed1c8ab14
protixer <gambler4o0o0@gmail.com>
2026-03-30 22:25:40 +0300

    Change file name and learning function, loops

--- Python Crash Course 3rd Edition - Eric Matthes/loops.py
+++ Python Crash Course 3rd Edition - Eric Matthes/loops.py
@@ -1,4 +1,23 @@
-cars = ["audi", "bmw", "subaru"]
+# cars = ["audi", "bmw", "subaru"]
+#
+# for car in cars:
+#     print(car)
 
-for car in cars:
-    print(car)
+
+def meditate(mana, max_mana, num_potions):
+    current_mana = mana
+    maximum_posible_mana = max_mana
+    numbers_of_potions = num_potions
+
+    while True:
+        if current_mana >= maximum_posible_mana:
+            break
+        current_mana += 1
+        if numbers_of_potions >= 0:
+            break
+        numbers_of_potions -= 1
+
+    return current_mana, numbers_of_potions
+
+
+print(meditate(0, 10, 9))
--- Python Crash Course 3rd Edition - Eric Matthes/numbers.py
+++
@@ -1,7 +0,0 @@
-big_number = 140_000_000
-print(big_number)
-
-digits = list(range(1,11))
-print(min(digits))
-print(max(digits))
-print(sum(digits))
