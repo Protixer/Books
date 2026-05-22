a44f0960f661d1b9b2a08ef592da85601c1c6925
protixer <gambler4o0o0@gmail.com>
2026-05-22 22:32:40 +0300

    Learning Python Dict and Set

--- Python Crash Course 3rd Edition - Eric Matthes/dictionary.py
+++ Python Crash Course 3rd Edition - Eric Matthes/dictionary.py
@@ -101,12 +101,27 @@
 #         language = favorite_languages[name].title()
 #         print(f"{name.title()} is {language.title()}")
 
-favorite_languages = {
-    "jen": "python",
-    "sarah": "c",
-    "edward": "rust",
-    "phil": "python",
-}
-print("The following languages have been mentioned:")
-for language in favorite_languages.values():
-    print(language.title())
+# favorite_languages = {
+#     "jen": "python",
+#     "sarah": "c",
+#     "edward": "rust",
+#     "phil": "python",
+# }
+# print("The following languages have been mentioned:")
+# for language in favorite_languages.values():
+#     print(language.title())
+
+
+# Make an empty list for storing aliens.
+# aliens = []
+# # Make 30 green aliens.
+# for alien_number in range(30):
+#     new_alien = {"color": "green", "points": 5, "speed": "slow"}
+#     aliens.append(new_alien)
+# # Show the first 5 aliens.
+# for alien in aliens[:5]:
+#     print(alien)
+#
+# print("...")
+# # Show how many aliens have been created.
+# print(f"Total number of aliens: {len(aliens)}")
